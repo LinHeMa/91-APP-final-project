866620bd69381126f6343d56da103af7a86516b6
Jay <chriese0112@gmail.com>
2022-10-28 16:51:15 +0800

    add photos

---
+++ co-work-project/src/data/productData.ts
@@ -0,0 +1,87 @@
+export const productData = [
+  { title: '全站商品', content: [] },
+  { title: '人氣商品推薦', content: [] },
+  { title: '最新活動', content: [] },
+  {
+    title: '9月限時活動',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '本月主題推薦',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '流行女鞋',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '手工婚鞋',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '包包系列',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '獨家聯名',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '貼心鞋材',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+  {
+    title: '選款式',
+    content: [
+      { title: 'NO.1冠軍小白鞋家族' },
+      { title: 'Kitty獨家聯名款' },
+      { title: '美樂蒂獨家聯名鞋款' },
+      { title: '美腿製造機涼鞋' },
+    ],
+  },
+];
+
+export const titleData = [
+  { title: '商品分類', content: [{ title: 'NO.1冠軍小白鞋家族' }] },
+  { title: 'Latest Promotions', content: [{ title: 'NO.1冠軍小白鞋家族' }] },
+  {
+    title: 'Crayon Shinchan 15% off',
+    content: [],
+  },
+  { title: 'Best Sellers', content: [] },
+];
