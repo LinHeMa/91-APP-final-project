99c87e7f8d96ea83054c119ef23aa4258227498d
linHeMa <linhema0921@gmail.com>
2022-10-28 17:53:35 +0800

    ReserveHeader_add_logo_H&W

--- co-work-project/src/components/ReserveHeader.tsx
+++ co-work-project/src/components/ReserveHeader.tsx
@@ -5,7 +5,7 @@ const ReserveHeader = () => {
       <img
         src={logo}
         alt="91AppLogo"
-        className="md:ml-[10.7%] ml-[4%] w-[75px] h-auto md:w-[90px]"
+        className="md:ml-[10.7%] ml-[4%] w-[75px] h-[21.69px] md:w-[90px] md:h-[26.03px]"
       />
     </div>
   );
--- co-work-project/src/pages/reservation/ReservationIndex.tsx
+++ co-work-project/src/pages/reservation/ReservationIndex.tsx
@@ -1,11 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import ReserveHeader from '../../components/ReserveHeader';
 
 const ReservationIndex = () => {
   return (
     <div>
       <ReserveHeader />
-      <Link to="/reservation/successpage">success</Link>
       <Outlet />
       <div>footer</div>
     </div>
