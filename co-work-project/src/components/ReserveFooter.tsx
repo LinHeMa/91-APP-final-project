439f9844b831c652505ca19ae57592a7a2263ce2
linHeMa <linhema0921@gmail.com>
2022-10-30 09:29:55 +0800

    ReserveFooter_add_RWD_layout

---
+++ co-work-project/src/components/ReserveFooter.tsx
@@ -0,0 +1,11 @@
+const ReserveFooter = () => {
+  return (
+    <div className="w-[100vw] flex h-[68px] justify-center items-center drop-shadow-sm absolute bottom-0">
+      <div className="w-[85%] h-[40px] md:h-[50px] bg-[#FF5353] rounded-[5px] flex justify-center items-center text-white max-w-[1080px]">
+        搶先登記
+      </div>
+    </div>
+  );
+};
+
+export default ReserveFooter;
