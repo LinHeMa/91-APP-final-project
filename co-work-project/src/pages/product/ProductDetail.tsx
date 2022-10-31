95e4b0940af6ee51d1fe23b6d47dd6b082a9105b
Jay <chriese0112@gmail.com>
2022-10-31 12:11:17 +0800

    add_header_route

--- co-work-project/src/pages/product/ProductDetail.tsx
+++ co-work-project/src/pages/product/ProductDetail.tsx
@@ -1,6 +1,9 @@
+import ProductHeader from '../../components/product/ProductHeader';
+
 const ProductDetail = () => {
   return (
     <div>
+      <ProductHeader />
       123 789
       <div>Edit by frog</div>
     </div>
