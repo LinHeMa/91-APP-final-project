163ba8e118d89bc9ffb5e1a0072291fa407bafda
linHeMa <linhema0921@gmail.com>
2022-10-31 15:00:18 +0800

    ReserveCarousel_add_drag_change_photo

--- co-work-project/src/components/ReserveCarousell.tsx
+++ co-work-project/src/components/ReserveCarousell.tsx
@@ -3,6 +3,10 @@ import carousellImg from '../data/carousellImg';
 import CarousellDot from './CarousellDot';
 import ChooseButton from './ChooseButton';
 import Label from './Label';
+interface touchEventTypes {
+  trace: number[];
+}
+
 const ReserveCarousell = () => {
   const [photoIndex, setPhotoIndex] = useState(0);
   const photoCount = (array: string[]) => array.length - 1;
@@ -15,6 +19,20 @@ const ReserveCarousell = () => {
     setPhotoIndex((prev) => prev - 1);
   };
   const timerRef = useRef<ReturnType<typeof setInterval>>();
+  const [mouseDownX, setMouseDownX] = useState(0);
+  const [touchEvent, setTouchEvent] = useState<touchEventTypes>({ trace: [] });
+  const dragItem = useRef<number>();
+  const dragStart = (e: React.DragEvent<HTMLDivElement>, position: number) => {
+    const target = e.target as HTMLInputElement;
+    dragItem.current = position;
+    console.log(target?.innerHTML);
+    console.log('drag');
+  };
+
+  const dragPhoto = (prevX: number, X: number) => {
+    if (X - prevX < 0) return nextPhoto();
+    if (X - prevX > 0) return prevPhoto();
+  };
   const addTimer = useCallback(() => {
     timerRef.current = setInterval(() => {
       setPhotoIndex((prev) => {
@@ -38,19 +56,45 @@ const ReserveCarousell = () => {
     <div
       className="flex flex-col justify-center items-center"
       onMouseEnter={() => {
-        console.log('in');
         clearInterval(timerRef.current);
       }}
       onMouseLeave={() => {
-        console.log('out');
+        addTimer();
+      }}
+      onMouseDown={(e) => {
+        setMouseDownX(e.pageX);
+        clearInterval(timerRef.current);
+      }}
+      onMouseUp={(e) => {
+        dragPhoto(mouseDownX, e.pageX);
+        addTimer();
+      }}
+      onTouchStart={(e) => {
+        clearInterval(timerRef.current);
+      }}
+      onTouchMove={(e) => {
+        setTouchEvent((prev) => {
+          return { trace: [...prev.trace, e.targetTouches[0].clientX] };
+        });
+      }}
+      onTouchEnd={(e) => {
+        const touchStartAt = touchEvent.trace[0];
+        const touchEndAt = touchEvent.trace[touchEvent.trace.length - 1];
+        dragPhoto(touchStartAt, touchEndAt);
         addTimer();
       }}
     >
-      <span className="relative  h-[424px] flex  justify-center items-center">
-        <span className=' absolute top-10 right-5'>
+      <span className="relative w-[359px] h-[424px] overflow-hidden flex  justify-center items-center">
+        <span className=" absolute top-10 right-5">
           <Label total={photoCount(carousellImg) + 1} index={photoIndex + 1} />
         </span>
-        <img src={carousellImg[photoIndex]} alt="iphone照片"/>
+        <img
+          src={carousellImg[photoIndex]}
+          alt="iphone照片"
+          className="min-w-[358px]"
+          draggable={true}
+          onDragStart={(e) => dragStart(e, photoIndex)}
+        />
         <span className="absolute top-[210px] right-0">
           <ChooseButton direction="left" clickFn={nextPhoto} />
         </span>
