7e3d17aebdca413a78f2ef34e10397cef07f8026
Jay <chriese0112@gmail.com>
2022-11-02 13:44:41 +0800

    add_one_breake_point

--- co-work-project/src/pages/reservation/Reservation.tsx
+++ co-work-project/src/pages/reservation/Reservation.tsx
@@ -7,7 +7,7 @@ const Reservation = () => {
     <div className=" flex-grow basis-auto flex-shrink-0 bg-[#FAFAFA] md:pb-[150px] pt-[30px] md:pt-[61px]">
       <div className=" md:flex md:flex-row-reverse md:justify-center md:flex-wrap md:ml-[-50px]">
         <div className="md:ml-[29px]">
-          <h1 className="font-bold text-[24px] text-center block h-10 md:h-[70px] md:mt-[83px] md:text-[48px] ">
+          <h1 className="font-bold text-[24px] text-center block h-10 md:h-[70px] lg:mt-[83px] md:text-[48px] ">
             iPhone新機搶先登記
           </h1>
           <span className="block text-center text-[#545454] text-[14px] font-medium md:text-[24px]  ">
@@ -18,7 +18,7 @@ const Reservation = () => {
             <br />
             每會員帳號限預約一支
           </span>
-          <div className="mt-5 px-5 mx-auto md:w-[571.94px] md:h-[145px]">
+          <div className="mt-5 px-5 max-w-[571.94px] mx-auto md:w-[571.94px] md:h-[145px]">
             <img src={flowImg} alt="flowImg" className="w-[100%] h-[100%]" />
           </div>
         </div>
