6d081a10f66627ee1833f83db24b6ef3046806b7
Jay <chriese0112@gmail.com>
2022-11-08 15:01:33 +0800

    modified_margin_and_add_one_props_to_component

--- co-work-project/src/components/product/ProductNav.tsx
+++ co-work-project/src/components/product/ProductNav.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import favImg from '../../assets/productImgs/favIcon.png';
 import shopcartImg from '../../assets/productImgs/shopcartIcon.png';
 import ProductCard from '../../components/product/ProductCard';
@@ -9,6 +10,7 @@ interface ProductNavProps {
   currentPrice: number;
   photo: string;
   status: string;
+  isLastData: boolean;
 }
 
 function ProductNav({
@@ -18,6 +20,7 @@ function ProductNav({
   currentPrice,
   photo,
   status,
+  isLastData,
 }: ProductNavProps) {
   return (
     <div className="pl-5">
@@ -31,16 +34,16 @@ function ProductNav({
           {status !== '' && <ProductCard status={status} />}
         </div>
         <div className="flex flex-col w-[190px]">
-          <span className=" h-5 text-[#999999] text-[14px] mb-1 cursor-pointer">
+          <span className=" h-5 text-[#999999] text-[14px] mb-1 cursor-pointer hover:underline">
             {owner}
           </span>
-          <span className="text-[14px] mb-[36px] h-[42px] line-clamp-2 overflow-hidden cursor-pointer">
+          <span className="text-[14px] mb-[36px] h-[42px] line-clamp-2 overflow-hidden cursor-pointer hover:text-[#ff655d]">
             {title}
           </span>
           <div className="flex justify-between items-end">
             <div className="flex flex-col h-[30px]  w-[92px] justify-end ">
-              <p className="line-through text-[12px] text-[#999999] ">{`NT$${prevPrice}`}</p>
-              <p className="text-[16px] text-[#ff655d] flex items-en">{`NT$${currentPrice}`}</p>
+              <p className="line-through text-[12px] text-[#999999] cursor-pointer">{`NT$${prevPrice}`}</p>
+              <p className="text-[16px] text-[#ff655d] flex  cursor-pointer">{`NT$${currentPrice}`}</p>
             </div>
             <div className="flex ">
               <div className="w-5 h-5 mr-3 mb-1">
@@ -57,7 +60,12 @@ function ProductNav({
           </div>
         </div>
       </div>
-      <div className="w-[93%] h-[1px] border-[1px] border-[#ececec] "></div>
+      <div
+        className={classNames({
+          'w-[93%] h-[1px] border-[1px] border-[#ececec]': true,
+          'border-0': isLastData,
+        })}
+      ></div>
     </div>
   );
 }
