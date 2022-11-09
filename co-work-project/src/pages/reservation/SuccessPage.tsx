4689adf03a6c5037a4b4acd96ee872ae2e5bb52f
Jay <chriese0112@gmail.com>
2022-11-09 17:07:43 +0800

    modified_margin

--- co-work-project/src/pages/reservation/SuccessPage.tsx
+++ co-work-project/src/pages/reservation/SuccessPage.tsx
@@ -19,22 +19,24 @@ const SuccessPage = () => {
   });
   return (
     <div className="flex flex-col justify-center items-center ">
-      <h1 className="w-[300px] h-[40px] text-[24px] font-bold  mt-[30px] flex justify-center items-center mb-[2px] md:text-[64px] md:w-[713px] md:h-[80px]">
+      <h1 className=" h-[40px] text-[24px] font-bold  mt-[30px] flex justify-center items-center mb-[2px] md:text-[64px] md:w-[713px] md:h-[80px]">
         恭喜您！驗證成功
       </h1>
       <div className="w-[191px] h-[152px]  mb-[2px] md:w-[294px] md:h-[235px]">
         <img className="w-[100%] h-[100%]" src={heartImg} alt="heartImg" />
       </div>
-      <span className="w-[250px] h-[48px] text-[#333333] text-center mb-[18px] md:text-[24px] md:h-auto md:w-[350px]">
+      <span className=" h-[48px] text-[#333333] text-center mb-[18px] md:text-[24px] md:h-auto md:w-[350px]">
         已完成 {state.model} 登記 <br />
         敬請等候開賣通知
       </span>
-      <span className="text-[#ff5353] text-center w-[342px] h-[54px] font-semibold mb-5 md:text-[24px] md:max-w-[1028px] md:w-auto md:px-[10px]">
-        注意：簡訊驗證通過才算有登記，完成登記的手機號碼需與會員手機號碼相同，每人限購一支
-      </span>
-      <div className="w-full md:bg-[#f2f2f2] ">
+      <div className="px-[25px] flex justify-center items-center">
+        <span className="text-[#ff5353] text-center font-semibold mb-5 md:text-[24px] md:max-w-[1028px] md:w-auto md:px-[10px]">
+          注意：簡訊驗證通過才算有登記，完成登記的手機號碼需與會員手機號碼相同，每人限購一支
+        </span>
+      </div>
+      <div className="w-full md:bg-[#f2f2f2] pb-[150px] md:pb-[60px]">
         <div className="h-[395px] bg-[#f2f2f2] pt-[30px]  w-full  md:h-[690px]">
-          <div className="w-full px-[25px] h-[348px] mx-auto md:w-auto md:max-w-[850px] md:px-[25px] leading-24 md:leading-48 text-[14px] md:text-[24px] px-2">
+          <div className="w-full px-[25px] h-[348px] mx-auto md:w-auto md:max-w-[850px] md:px-[25px] leading-24 md:leading-48 text-[14px] md:text-[24px] ">
             <span className="font-bold w-[98px] h-[20px] ">登記資訊如下：</span>
             <div className="border-b-[1px] border-b-[#D8D8D8] mt-[9px]" />
             <div className=" h-[304px] mt-[15px]  w-full">
